a20977344655f16162122ecdd42156e611f144f9
vignesh14052002 <vignesharivazhagan1409@gmail.com>
2022-11-26 10:00:38 +0530

    pwa for portfolio

--- portfolio/serviceworker.js
+++ portfolio/serviceworker.js
@@ -4,7 +4,7 @@ var staticCacheName = "pwa";
 self.addEventListener("install", function (e) {
   e.waitUntil(
     caches.open(staticCacheName).then(function (cache) {
-      return cache.addAll(["/"]);
+      return cache.addAll(["/frontend/portfolio/"]);
     })
   );
 });
